20ed04afb08646a822e88bbc28ed0c7dd71933e5
Stefan Partheymüller <stefan-partheymueller@gmx.de>
2014-05-26 00:38:41 +0200

    extended codeblock lexer

--- scite/lex/codeblock.lexer.lua
+++ scite/lex/codeblock.lexer.lua
@@ -11,11 +11,16 @@ function OnStyle(styler)
     S_IDENTIFIER     = 1
     S_KEYWORD        = 2
     S_COMMENT        = 3
+    S_STRING         = 4
+    S_OPERATOR       = 5
+    S_DECLARATION    = 6
     
     keywords         = StringSplit(props["keywords.$(file.patterns.codeblock)"],
                                    " ")
-    identifier_chars = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    
+    identifier_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ..
+                       "_1234567890"
+    operator_chars   = "-+*/=<>(){}|:,"
+    newline_chars    = "\r\n"
     
     ----------------------------------------------------------------------------
     styler:StartStyling(styler.startPos, styler.lengthDoc, styler.initStyle)
@@ -26,13 +31,32 @@ function OnStyle(styler)
             if not identifier_chars:find(styler:Current(), 1, true) then
                 identifier = styler:Token()
                 if TableContainsKey(keywords, identifier) then
+                    -- Use ChangeState() here istead of SetState(), since we
+                    -- need to change the state of the CURRENT token (which is
+                    -- a keyword)
                     styler:ChangeState(S_KEYWORD)
                 end
+                
                 styler:SetState(S_DEFAULT)
             end
-        elseif styler:State() == S_COMMENT then
-            if styler:Match("\n") then
+        elseif styler:State() == S_STRING then
+            if styler:Current() == "'" then
+                if styler:Next() == "'" then
+                    styler:Forward() -- Skip ...
+                else
+                    styler:ForwardSetState(S_DEFAULT)
+                end
+            end
+        elseif styler:State() == S_OPERATOR then
+            styler:SetState(S_DEFAULT)
+        elseif styler:State() == S_DECLARATION then
+            if styler:Current() == "|" then
                 styler:ForwardSetState(S_DEFAULT)
+            end
+        elseif styler:State() == S_COMMENT then
+            if newline_chars:find(styler:Current(), 1, true) or
+               styler:Match(newline_chars) then
+                styler:SetState(S_DEFAULT)
             end
         end
         
@@ -40,6 +64,13 @@ function OnStyle(styler)
         if styler:State() == S_DEFAULT then
             if styler:Match("//") then
                 styler:SetState(S_COMMENT)
+            elseif styler:Current() == "'" then
+                styler:SetState(S_STRING)
+            elseif operator_chars:find(styler:Current(), 1, true) then
+                styler:SetState(S_OPERATOR)
+                if styler:Current() == "|" then
+                    styler:SetState(S_DECLARATION)
+                end
             elseif identifier_chars:find(styler:Current(), 1, true) then
                 styler:SetState(S_IDENTIFIER)
             end
